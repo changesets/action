f2660aa7e78365f53dbeb4cfa774c1499ec6483a
Ernesto García <ernestognw@gmail.com>
2024-03-18 22:57:05 +0000

    Feature: Allow custom `branch` (#255)

--- src/run.ts
+++ src/run.ts
@@ -299,6 +299,7 @@ type VersionOptions = {
   commitMessage?: string;
   hasPublishScript?: boolean;
   prBodyMaxCharacters?: number;
+  branch?: string;
 };
 
 type RunVersionResult = {
@@ -313,11 +314,12 @@ export async function runVersion({
   commitMessage = "Version Packages",
   hasPublishScript = false,
   prBodyMaxCharacters = MAX_CHARACTERS_PER_MESSAGE,
+  branch,
 }: VersionOptions): Promise<RunVersionResult> {
   const octokit = setupOctokit(githubToken);
 
   let repo = `${github.context.repo.owner}/${github.context.repo.repo}`;
-  let branch = github.context.ref.replace("refs/heads/", "");
+  branch = branch ?? github.context.ref.replace("refs/heads/", "");
   let versionBranch = `changeset-release/${branch}`;
 
   let { preState } = await readChangesetState(cwd);
