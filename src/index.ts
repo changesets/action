8d0319a76835b78da4d4fa99de68d925667337ee
Joshua Pendragon <pendragon.josh@gmail.com>
2024-09-19 15:41:25 -0500

    refactor action to parameterize the registry

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,15 @@ import readChangesetState from "./readChangesetState";
 const getOptionalInput = (name: string) => core.getInput(name) || undefined;
 
 (async () => {
-  let githubToken = process.env.GITHUB_TOKEN;
+  try {
+    await main();
+  } catch (err) {
+    handleError(err);
+  }
+})();
 
+async function main() {
+  const githubToken = process.env.GITHUB_TOKEN || "";
   if (!githubToken) {
     core.setFailed("Please add the GITHUB_TOKEN to the changesets action");
     return;
@@ -16,107 +23,139 @@ const getOptionalInput = (name: string) => core.getInput(name) || undefined;
 
   const inputCwd = core.getInput("cwd");
   if (inputCwd) {
-    core.info("changing directory to the one given as the input");
     process.chdir(inputCwd);
+    core.info("Changing directory to the one given as the input");
   }
 
-  let setupGitUser = core.getBooleanInput("setupGitUser");
-
+  const setupGitUser = core.getBooleanInput("setupGitUser");
   if (setupGitUser) {
-    core.info("setting git user");
     await gitUtils.setupUser();
+    core.info("Setting git user");
   }
 
-  core.info("setting GitHub credentials");
-  await fs.writeFile(
-    `${process.env.HOME}/.netrc`,
-    `machine github.com\nlogin github-actions[bot]\npassword ${githubToken}`
-  );
+  await setGitHubCredentials(githubToken);
 
-  let { changesets } = await readChangesetState();
-
-  let publishScript = core.getInput("publish");
-  let hasChangesets = changesets.length !== 0;
-  const hasNonEmptyChangesets = changesets.some(
-    (changeset) => changeset.releases.length > 0
-  );
-  let hasPublishScript = !!publishScript;
+  const { changesets } = await readChangesetState();
+  const publishScript = core.getInput("publish");
+  const hasChangesets = changesets.length !== 0;
+  const hasNonEmptyChangesets = changesets.some(changeset => changeset.releases.length > 0);
+  const hasPublishScript = !!publishScript;
 
   core.setOutput("published", "false");
   core.setOutput("publishedPackages", "[]");
   core.setOutput("hasChangesets", String(hasChangesets));
 
-  switch (true) {
-    case !hasChangesets && !hasPublishScript:
-      core.info("No changesets found");
-      return;
-    case !hasChangesets && hasPublishScript: {
-      core.info(
-        "No changesets found, attempting to publish any unpublished packages to npm"
-      );
+  await handleChangesetCases(hasChangesets, hasNonEmptyChangesets, hasPublishScript, publishScript, githubToken);
+}
 
-      let userNpmrcPath = `${process.env.HOME}/.npmrc`;
-      if (fs.existsSync(userNpmrcPath)) {
-        core.info("Found existing user .npmrc file");
-        const userNpmrcContent = await fs.readFile(userNpmrcPath, "utf8");
-        const authLine = userNpmrcContent.split("\n").find((line) => {
-          // check based on https://github.com/npm/cli/blob/8f8f71e4dd5ee66b3b17888faad5a7bf6c657eed/test/lib/adduser.js#L103-L105
-          return /^\s*\/\/registry\.npmjs\.org\/:[_-]authToken=/i.test(line);
-        });
-        if (authLine) {
-          core.info(
-            "Found existing auth token for the npm registry in the user .npmrc file"
-          );
-        } else {
-          core.info(
-            "Didn't find existing auth token for the npm registry in the user .npmrc file, creating one"
-          );
-          fs.appendFileSync(
-            userNpmrcPath,
-            `\n//registry.npmjs.org/:_authToken=${process.env.NPM_TOKEN}\n`
-          );
-        }
-      } else {
-        core.info("No user .npmrc file found, creating one");
-        fs.writeFileSync(
-          userNpmrcPath,
-          `//registry.npmjs.org/:_authToken=${process.env.NPM_TOKEN}\n`
-        );
-      }
-
-      const result = await runPublish({
-        script: publishScript,
-        githubToken,
-        createGithubReleases: core.getBooleanInput("createGithubReleases"),
-      });
-
-      if (result.published) {
-        core.setOutput("published", "true");
-        core.setOutput(
-          "publishedPackages",
-          JSON.stringify(result.publishedPackages)
-        );
-      }
-      return;
-    }
-    case hasChangesets && !hasNonEmptyChangesets:
-      core.info("All changesets are empty; not creating PR");
-      return;
-    case hasChangesets:
-      const { pullRequestNumber } = await runVersion({
-        script: getOptionalInput("version"),
-        githubToken,
-        prTitle: getOptionalInput("title"),
-        commitMessage: getOptionalInput("commit"),
-        hasPublishScript,
-        branch: getOptionalInput("branch"),
-      });
-
-      core.setOutput("pullRequestNumber", String(pullRequestNumber));
-
-      return;
+async function handleChangesetCases(hasChangesets: boolean, hasNonEmptyChangesets: boolean, hasPublishScript: boolean, publishScript: string, githubToken: string) {
+  if (!hasChangesets && !hasPublishScript) {
+    await handleNoChangesetsNoPublishScript();
+  } else if (!hasChangesets && hasPublishScript) {
+    await handleNoChangesetsHasPublishScript(publishScript, githubToken);
+  } else if (hasChangesets && !hasNonEmptyChangesets) {
+    await handleEmptyChangesets();
+  } else if (hasChangesets) {
+    await handleHasChangesets(githubToken, hasPublishScript);
   }
-})().catch((err) => {
-  core.error(err);
-  core.setFailed(err.message);
-});
+}
+
+async function handleNoChangesetsNoPublishScript() {
+  core.info("No changesets found");
+}
+
+async function handleNoChangesetsHasPublishScript(
+  publishScript: string,
+  githubToken: string
+) {
+  core.info("No changesets found, attempting to publish any unpublished packages to npm");
+  await setupNpmrc();
+  const result = await runPublish({
+    script: publishScript,
+    githubToken,
+    createGithubReleases: core.getBooleanInput("createGithubReleases"),
+  });
+
+  if (result.published) {
+    core.setOutput("published", "true");
+    core.setOutput(
+      "publishedPackages",
+      JSON.stringify(result.publishedPackages)
+    );
+  }
+}
+
+async function handleEmptyChangesets() {
+  core.info("All changesets are empty; not creating PR");
+}
+
+async function handleHasChangesets(
+  githubToken: string,
+  hasPublishScript: boolean
+) {
+  const versionResult = await runVersion({
+    script: getOptionalInput("version"),
+    githubToken,
+    prTitle: getOptionalInput("title"),
+    commitMessage: getOptionalInput("commit"),
+    hasPublishScript,
+    branch: getOptionalInput("branch"),
+  });
+
+  core.setOutput("pullRequestNumber", String(versionResult.pullRequestNumber));
+}
+
+async function setGitHubCredentials(githubToken: string) {
+  core.info("Setting GitHub credentials");
+  await fs.writeFile(
+    `${process.env.HOME}/.netrc`,
+    `machine github.com\nlogin github-actions[bot]\npassword ${githubToken}`
+  );
+}
+
+function handleError(err: unknown) {
+  if (err instanceof Error) {
+    core.error(err);
+    core.setFailed(err.message);
+  }
+}
+
+async function setupNpmrc() {
+  const userNpmrcPath = `${process.env.HOME}/.npmrc`;
+  const registry = core.getInput("registry");
+  if (fs.existsSync(userNpmrcPath)) {
+    await processExisting(userNpmrcPath, registry);
+  } else {
+    createNpmrcFile(userNpmrcPath,registry);
+  }
+}
+
+async function processExisting(userNpmrcPath: string, registry:string) {
+  const userNpmrcContent = await fs.readFile(userNpmrcPath, "utf8");
+  const authLine = userNpmrcContent
+    .split("\n")
+    .find((line) =>
+      new RegExp(`^\\s*//${registry}/:[_-]authToken=`, "i").test(line)
+    );
+  if (!authLine) {
+    core.info(
+      "Didn't find existing auth token for the npm registry in the user .npmrc file, creating one"
+    );
+    fs.appendFileSync(
+      userNpmrcPath,
+      `\n//${registry}/:_authToken=${process.env.NPM_TOKEN}\n`
+    );
+  } else {
+    core.info(
+      "Found existing auth token for the npm registry in the user .npmrc file"
+    );
+  }
+}
+
+function createNpmrcFile(userNpmrcPath: string, registry:string) {
+  core.info("No user .npmrc file found, creating one");
+  fs.writeFileSync(
+    userNpmrcPath,
+    `//${registry}/:_authToken=${process.env.NPM_TOKEN}\n`
+  );
+}
