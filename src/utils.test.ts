e82f42c2cc383535bb2fdaaf65164181ea6f9cdb
Mateusz Burzyński <mateuszburzynski@gmail.com>
2025-05-11 20:59:28 +0200

    Migrate to Vitest (#483)
    * Migrate to Vitest
    
    * `skipLibCheck` for now

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,4 +1,5 @@
-import { getChangelogEntry, BumpLevels, sortTheThings } from "./utils";
+import { expect, test } from "vitest";
+import { BumpLevels, getChangelogEntry, sortTheThings } from "./utils";
 
 let changelog = `# @keystone-alpha/email
 
