859224395622426b3e105b0803a69bea3d577022
bluwy <bjornlu.dev@gmail.com>
2026-05-25 16:10:22 +0800

    Run format lint in CI

--- rolldown.config.js
+++ rolldown.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from 'rolldown'
+import { defineConfig } from "rolldown";
 
 export default defineConfig({
-  input: 'src/index.ts',
+  input: "src/index.ts",
   output: {
-    dir: 'dist',
-    format: 'esm',
+    dir: "dist",
+    format: "esm",
     cleanDir: true,
     minify: true,
     comments: false,
   },
-  platform: 'node',
-})
+  platform: "node",
+});
