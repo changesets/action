1e25d9ffc5d468853017ae74c3fb7d47a7bd017a
bluwy <bjornlu.dev@gmail.com>
2026-05-28 19:15:31 +0800

    Update rolldown config

--- rolldown.config.js
+++ rolldown.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from "rolldown";
 
 export default defineConfig({
-  input: "src/index.ts",
+  input: {
+    index: "src/index.ts",
+    ["pr-comment"]: "src/pr-comment/index.ts",
+  },
   output: {
     dir: "dist",
     format: "esm",
