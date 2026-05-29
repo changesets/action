2ae2cfe8fb8ce5b0dd472d7fab775b8538d4437d
bluwy <bjornlu.dev@gmail.com>
2026-05-29 20:32:22 +0800

    fix merge

--- rolldown.config.js
+++ rolldown.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "rolldown";
 
 export default defineConfig({
   input: {
-    index: {
     index: "src/index.ts",
     ["pr-status"]: "src/pr-status/index.ts",
     ["pr-comment"]: "src/pr-comment/index.ts",
