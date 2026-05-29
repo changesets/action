142abaec219f26a004e604febb1d63bb239b3824
bluwy <bjornlu.dev@gmail.com>
2026-05-29 15:11:54 +0800

    Use TLA

--- src/pr-status/index.ts
+++ src/pr-status/index.ts
@@ -2,20 +2,22 @@ import * as core from "@actions/core";
 import * as github from "@actions/github";
 import { getCommentMessage } from "./message.ts";
 
-(async () => {
+try {
+  await main();
+} catch (err) {
+  core.setFailed((err as Error).message);
+}
+
+async function main() {
   const context = github.context.payload.pull_request;
   if (!context) {
-    core.error(
+    throw new Error(
       "This action should only be run on `pull_request_target` or `pull_request` events",
     );
-    return;
   }
 
   core.info("Creating comment message...");
   const commentBody = await getCommentMessage(context);
   core.setOutput("comment-body", commentBody);
   core.info("Done!");
-})().catch((err) => {
-  core.error(err);
-  core.setFailed(err.message);
-});
+}
