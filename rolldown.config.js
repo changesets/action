93abc0e48ceb8f91749579e501c394eb3e751261
bluwy <bjornlu.dev@gmail.com>
2026-06-22 09:09:41 +0800

    update rolldown config

--- rolldown.config.js
+++ rolldown.config.js
@@ -4,9 +4,9 @@ export default defineConfig({
   input: {
     index: "src/index.ts",
     pack: "src/pack/index.ts",
-    ["pr-status"]: "src/pr-status/index.ts",
-    ["pr-comment"]: "src/pr-comment/index.ts",
-    ["select-mode"]: "src/select-mode/index.ts",
+    "pr-status": "src/pr-status/index.ts",
+    "pr-comment": "src/pr-comment/index.ts",
+    "select-mode": "src/select-mode/index.ts",
     version: "src/version/index.ts",
     publish: "src/publish/index.ts",
   },
