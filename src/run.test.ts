5a747ed99088ef9f5d3cb08b8ab35c3ab7c5501c
Mateusz Burzyński <mateuszburzynski@gmail.com>
2026-07-22 11:27:14 +0200

    better spy/mocking

--- src/run.test.ts
+++ src/run.test.ts
@@ -21,10 +21,8 @@ vi.mock("@actions/github", () => ({
     graphql: mockedGraphql,
   }),
 }));
-vi.mock("@actions/core", async (importOriginal) => ({
-  ...(await importOriginal<typeof import("@actions/core")>()),
-  warning: vi.fn(),
-}));
+vi.mock("@actions/core", { spy: true })
+const mockedWarning = vi.mocked(core.warning);
 vi.mock("@changesets/ghcommit");
 
 let mockedGithubMethods = {
@@ -126,7 +124,7 @@ describe("publish", () => {
     });
 
     expect(result).toEqual({ published: false, exitCode: 0 });
-    expect(core.warning).toHaveBeenCalledWith(
+    expect(mockedWarning).toHaveBeenCalledWith(
       expect.stringContaining(
         "GitHub releases and git tags cannot be created without this output",
       ),
