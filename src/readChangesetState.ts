a67fc96115e0f7b2a59a0d280a108db55522796e
bluwy <bjornlu.dev@gmail.com>
2026-08-05 17:19:47 +0800

    Handle pre change

--- src/readChangesetState.ts
+++ src/readChangesetState.ts
@@ -14,11 +14,9 @@ export default async function readChangesetState(
   let changesets = await readChangesets(cwd);
 
   if (preState !== undefined && preState.mode === "pre") {
-    let changesetsToFilter = new Set(preState.changesets);
-
     return {
       preState,
-      changesets: changesets.filter((x) => !changesetsToFilter.has(x.id)),
+      changesets: changesets.filter((x) => !x.id.startsWith("pre/")),
     };
   }
 
