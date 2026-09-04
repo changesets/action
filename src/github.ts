97d28e9bdaa91ccca45c736ea8f3e331ffc3779b
bluwy <bjornlu.dev@gmail.com>
2026-09-04 16:24:09 +0800

    Handle error when pushing git tags with the git CLI

--- src/github.ts
+++ src/github.ts
@@ -213,25 +213,26 @@ export class GitHub {
   }
 
   async pushTag(tag: string) {
-    if (!this.pushWithGitCli) {
-      return this.octokit.rest.git
-        .createRef({
+    try {
+      if (!this.pushWithGitCli) {
+        await this.octokit.rest.git.createRef({
           ...context.repo,
           ref: `refs/tags/${tag}`,
           sha: context.sha,
-        })
-        .catch((err) => {
-          // Assuming tag was manually pushed in custom publish script
-          core.warning(`Failed to create tag ${tag}: ${err.message}`);
         });
+      } else {
+        await exec("git", ["push", "origin", tag], {
+          cwd: this.cwd,
+          env: {
+            ...process.env,
+            ...(await this.#getCliAuthEnv()),
+          } as Record<string, string>,
+        });
+      }
+    } catch (err) {
+      // Assuming tag was manually pushed in custom publish script
+      core.warning(`Failed to create tag ${tag}: ${(err as Error).message}`);
     }
-    await exec("git", ["push", "origin", tag], {
-      cwd: this.cwd,
-      env: {
-        ...process.env,
-        ...(await this.#getCliAuthEnv()),
-      } as Record<string, string>,
-    });
   }
 
   async prepareBranch(branch: string) {
